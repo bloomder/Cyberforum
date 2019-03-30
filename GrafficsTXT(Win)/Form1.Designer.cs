69445d71bebba0cf1e1883338c32a53cb9a9b332
bloomder <bloomder@mail.ru>
2019-03-30 15:36:51 +0300

    Add Solution with Graffics, Task, Read txt file methods

---
+++ GrafficsTXT(Win)/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GrafficsTXT_Win_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(55, 203);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(24, 39);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Ось Y";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Visible = false;
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 203);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(37, 39);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            title2.Name = "Title1";
+            title2.Text = "Ось Z";
+            this.chart2.Titles.Add(title2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 109);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ GrafficsTXT(Win)/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrafficsTXT_Win_
+{
+    public partial class Form1 : Form
+    {
+
+        string file, str,st;
+        StreamReader sr;
+        int count = 0, cou = 0;
+        Task task;
+        Info info;
+        struct Info
+        {
+            public float X;
+            public float Y;
+            public float Z;
+        }
+        public Form1()
+        {            
+            InitializeComponent();
+        }
+        public void Graffics()
+        {
+            if(chart1.InvokeRequired)
+            {
+                chart1.Invoke(new Action(() => chart1.Series[0].Points.AddXY(info.X, info.Y)));
+            }
+            else
+            {
+                chart1.Series[0].Points.AddXY(info.X, info.Y);
+            }
+            if (chart2.InvokeRequired)
+            {
+                chart2.Invoke(new Action(() => chart2.Series[0].Points.AddXY(info.X, info.Z)));
+            }
+            else
+            {
+                chart2.Series[0].Points.AddXY(info.X, info.Z);
+            }
+        }
+        public void Method()
+        {            
+            sr = new StreamReader(file, Encoding.Default);
+            info = new Info();cou = 0;
+            while ((str = sr.ReadLine()) != null)
+            {
+                cou++;
+                count = 0; st = ""; str = str.Replace(".", ",");
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ((str[i].Equals(' ')) || (str[i].Equals('\t')) || (i == (str.Length - 1)))
+                    {
+                        if ((i == (str.Length - 1))) st += str[i];
+                        switch (count)
+                        {
+                            case 0:
+                                info.X = Convert.ToSingle(st); st = "";
+                                break;
+                            case 1:
+                                info.Y = Convert.ToSingle(st); st = "";
+                                break;
+                            case 2:
+                                info.Z = Convert.ToSingle(st); st = "";
+                                break;
+                        }
+                        count++;
+                    }
+                    else
+                    {
+                        st += str[i];
+                    }
+                }
+                Graffics();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            file = openFileDialog1.FileName;
+            task = new Task(Method);
+            button1.Visible = false;
+            task.Start();
+            //task.Wait();
+            this.WindowState = FormWindowState.Maximized;
+            button1.Location = new Point(0,0);
+            //-----------
+            chart1.Location = new Point(10, 30);
+            chart1.Width = this.Width - 50;
+            chart1.Height = 300;
+            chart1.Visible = true;
+            //------------
+            chart2.Location = new Point(10, 400);
+            chart2.Width = this.Width - 50;
+            chart2.Height = 300;
+            button1.Visible = true;
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+        }
+    }
+}
