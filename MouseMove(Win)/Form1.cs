b08d5b7d29a6b9a188f149c333659b5942fe21c0
bloomder <bloomder@mail.ru>
2019-03-30 12:53:44 +0300

    add solution MouseMove(Win)

---
+++ MouseMove(Win)/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace MouseMove_Win_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {            
+            label1.Text = "X-" + e.X.ToString() + " Y-" + e.Y.ToString();
+        }
+    }
+}
