8fd78a92c265aebdf8b1c01558e14e087b1193b5
bloomder <bloomder@mail.ru>
2019-03-31 14:16:17 +0300

    Add work BinWriteRead2

---
+++ BinWriteRead2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace BinWriteRead2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DataName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataCapital = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataArea = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DataPeople = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.DataName,
+            this.DataCapital,
+            this.DataArea,
+            this.DataPeople});
+            this.dataGridView1.Location = new System.Drawing.Point(18, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(460, 346);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // DataName
+            // 
+            this.DataName.HeaderText = "Name";
+            this.DataName.Name = "DataName";
+            // 
+            // DataCapital
+            // 
+            this.DataCapital.HeaderText = "Capital";
+            this.DataCapital.Name = "DataCapital";
+            // 
+            // DataArea
+            // 
+            this.DataArea.HeaderText = "Area";
+            this.DataArea.Name = "DataArea";
+            // 
+            // DataPeople
+            // 
+            this.DataPeople.HeaderText = "People";
+            this.DataPeople.Name = "DataPeople";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 425);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataCapital;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataArea;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DataPeople;
+    }
+}
+
---
+++ BinWriteRead2/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BinWriteRead2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Test tst = new Test();
+            Test[] tests = new Test[16];
+            tests[0] = new Test("Germany", "Berlin", 357168, 80.8);
+            tests[1] = new Test("France", "Paris", 640679, 64.7);
+            tests[2] = new Test("Germany1", "Berlin1", 357168, 80.8);
+            tests[3] = new Test("France1", "Paris1", 640679, 64.7);
+            tests[4] = new Test("Germany2", "Berlin2", 357168, 80.8);
+            tests[5] = new Test("France2", "Paris2", 640679, 64.7);
+            tests[6] = new Test("Germany3", "Berlin3", 357168, 80.8);
+            tests[7] = new Test("France3", "Paris3", 640679, 64.7);
+            tests[8] = new Test("Germany4", "Berlin4", 357168, 80.8);
+            tests[9] = new Test("France4", "Paris4", 640679, 64.7);
+            tests[10] = new Test("Germany5", "Berlin5", 357168, 80.8);
+            tests[11] = new Test("France5", "Paris5", 640679, 64.7);
+            tests[12] = new Test("Germany6", "Berlin6", 357168, 80.8);
+            tests[13] = new Test("France6", "Paris6", 640679, 64.7);
+            tests[14] = new Test("Germany7", "Berlin7", 357168, 80.8);
+            tests[15] = new Test("France7", "Paris7", 640679, 64.7);
+            string path = @"C:\test.dat";
+            BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create));
+            foreach (Test s in tests)
+            {
+                writer.Write(s.Name);
+                writer.Write(s.Capital);
+                writer.Write(s.Area);
+                writer.Write(s.People);
+            }
+            writer.Close();
+            BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open));
+            long count = 0;
+            while (reader.PeekChar() > -1)
+            {
+                tst.Name = reader.ReadString();
+                tst.Capital = reader.ReadString();
+                tst.Area = reader.ReadInt32();
+                tst.People = reader.ReadDouble();
+                count++;
+                dataGridView1.Rows.Add(tst.Name, tst.Capital, tst.Area, tst.People);
+            }
+            reader.Close();
+        }
+    }
+    class Test
+    {
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public int Area { get; set; }
+        public double People { get; set; }
+        public Test(){}
+        public Test(string name, string capital, int area, double people)
+        {
+            Name = name;
+            Capital = capital;
+            Area = area;
+            People = people;
+        }
+    }
+}
