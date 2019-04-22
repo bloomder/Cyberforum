8894038c574774b6edc12e922de7a046a6cb993c
Rinat <bloomder@mail.ru>
2019-04-22 23:46:19 +0300

    Добавлено несколько проектов

---
+++ BindingSourceFilter/DataInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace BindingSourceFilter
+{
+    public class DataInfo
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public DataInfo(string a, int b)
+        {
+            Name = a;
+            Year = b;
+        }
+    }
+}
---
+++ BindingSourceFilter/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BindingSourceFilter
+{
+    public partial class Form1 : Form
+    {
+        List<DataInfo> list;
+        Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+            list = new List<DataInfo>();
+            rand = new Random();
+            dataGridView.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Name", HeaderText = "Имя" });
+            dataGridView.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Year", HeaderText = "Год" });
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add( new DataInfo( RandomClass.Names[rand.Next(0,99)] , rand.Next(1980,2000) ) );
+            }
+            dataGridView.DataSource = list;
+        }
+    }    
+}
