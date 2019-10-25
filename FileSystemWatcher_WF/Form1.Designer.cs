72641c3771ea3668782972a5690781ef7886f781
Rinat <bloomder@mail.ru>
2019-10-25 15:30:47 +0300

    add changes

--- FileSystemWatcher_WF/Form1.Designer.cs
+++ FileSystemWatcher_WF/Form1.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 239);
+            this.button1.Location = new System.Drawing.Point(12, 228);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(113, 23);
             this.button1.TabIndex = 0;
@@ -55,7 +57,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(298, 239);
+            this.button2.Location = new System.Drawing.Point(298, 228);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(111, 23);
             this.button2.TabIndex = 2;
@@ -64,11 +66,31 @@
             this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Мыло куда отправлять";
+            this.label1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(425, 310);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
@@ -76,6 +98,7 @@
             this.Text = "Form1";
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -84,6 +107,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FileSystemWatcher_WF/Form1.cs
+++ FileSystemWatcher_WF/Form1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Security.AccessControl;
 using System.Threading;
 using System.Windows.Forms;
@@ -18,7 +21,14 @@ namespace FileSystemWatcher_WF
         string str_txt = "";
         FileWriteClass fwc = new FileWriteClass();
         int count = 0;
-
+        int flag = 0;
+        string smtp= "smtp.mail.ru";
+        string from_mail= "ptrivnv89@mail.ru";
+        string pass_mail="Roketa2020";
+        string to_mail="";
+        string header="";
+        string message="";
+        string att_file="";
         public Form1()
         {
             InitializeComponent();
@@ -26,16 +36,53 @@ namespace FileSystemWatcher_WF
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(fbd.ShowDialog()==DialogResult.OK) { list_folder_fisrt.Add(fbd.SelectedPath); richTextBox1.Text += (string)(fbd.SelectedPath + "\r\n"); }
+            if(fbd.ShowDialog()==DialogResult.OK) 
+            {
+                if (list_folder_fisrt.Count == 0)
+                {
+                    list_folder_fisrt.Add(fbd.SelectedPath);
+                    richTextBox1.Text += (string)(fbd.SelectedPath + "\r\n");
+                }
+                else
+                {
+                    flag = 0;
+                    foreach (var item in list_folder_fisrt)
+                    {
+                        if (fbd.SelectedPath.Equals(item)) flag = 1; break;
+                    }
+                    if(flag == 0) 
+                    {
+                        list_folder_fisrt.Add(fbd.SelectedPath);
+                        richTextBox1.Text += (string)(fbd.SelectedPath + "\r\n");
+                    }
+                    else { flag = 0; }
+                }                 
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.Visible = false; button1.Visible = false; button2.Visible = false; richTextBox1.Visible = false;
-            Thread th = new Thread(ThreadMethod);
-            if (StartSettings()) { th.Start(); }
-            else { MessageBox.Show("Вы не выбрали папки!!!"); this.Visible = true; button1.Visible = true; button2.Visible = true; richTextBox1.Visible = true; }
+            if (textBox1.Text == "" || textBox1.Text == null) { MessageBox.Show("Вы не заполнили поле мыла!!!"); }
+            else
+            {
+                to_mail = textBox1.Text;
+                OffVisible();
+                Thread th = new Thread(ThreadMethod);
+                if (StartSettings()) { th.Start(); }
+                else { MessageBox.Show("Вы не выбрали папки!!!"); OffVisible(); }
+            }
         }
+
+        private void OffVisible()
+        {
+            this.Visible = false; 
+            button1.Visible = false;
+            button2.Visible = false;
+            richTextBox1.Visible = false;
+            label1.Visible = false;
+            textBox1.Visible = false;
+        }
+
         private void ThreadMethod()
         {
             foreach (var item in list_folder_fisrt)
@@ -73,20 +120,26 @@ namespace FileSystemWatcher_WF
 
         private void OnRenamed(object sender, RenamedEventArgs e)
         {
+            /*
             count++;
             str_txt = count + ") Папка: " + e.FullPath + "\r\n" + "Файл: " + e.OldName + " переименован в " + e.Name + " " + DateTime.Now + ".\r\n";
             fwc.OpenText();
             fwc.WriteText(str_txt);
             fwc.CloseText();
+            */
             int yp = 0;
         m1: { }
             try
             {
+                /*
                 File.Copy(e.FullPath, (string)(@"C:\Result\"+count.ToString()+".q"));
                 str_txt = "Удалось сохранить\r\n";
                 fwc.OpenText();
                 fwc.WriteText(str_txt);
                 fwc.CloseText();
+                */
+                SendMail(smtp,from_mail,pass_mail, to_mail,"Файл переименован", (string)(e.OldFullPath+"\r\n"+e.FullPath.ToString()),e.FullPath.ToString());
+                //SendMail("smtp.yandex.ru", "почта", "пароль", "получатель", "заголовок", "текст");
             }
             catch (Exception ex)
             {
@@ -94,39 +147,50 @@ namespace FileSystemWatcher_WF
                 if (yp != 10) { goto m1; }
                 else
                 {
+                    /*
                     str_txt = "Не удалось сохранить\r\n";
                     fwc.OpenText();
                     fwc.WriteText(str_txt);
                     fwc.CloseText();
+                    */
+                    SendMail(smtp, from_mail, pass_mail, to_mail, "Не получилось отправить файл", e.FullPath.ToString());
                 }
             }
         }
 
         private void OnDeleted(object sender, FileSystemEventArgs e)
         {
-            //count++;
+            //count++;            
             str_txt = "Папка: " + e.FullPath + "\r\n" + "Файл: " + e.Name + " удален." + DateTime.Now +"\r\n";
+            /*
             fwc.OpenText();
             fwc.WriteText(str_txt);
-            fwc.CloseText();            
+            fwc.CloseText();    
+            */
+            SendMail(smtp, from_mail, pass_mail, to_mail, "Файл Удален", e.FullPath.ToString());
         }
 
         private void OnCreated(object sender, FileSystemEventArgs e)
         {
+            /*
             count++;
             str_txt = count + ") Папка: " + e.FullPath + "\r\n" + "Файл: " + e.Name + " создан." + DateTime.Now + "\r\n";
             fwc.OpenText();
             fwc.WriteText(str_txt);
             fwc.CloseText();
+            */
             int yp = 0;
         m1: { }
             try
             {
+                /*
                 File.Copy(e.FullPath, (string)(@"C:\Result\" + count.ToString() + ".q"));
                 str_txt = "Удалось сохранить\r\n";
                 fwc.OpenText();
                 fwc.WriteText(str_txt);
                 fwc.CloseText();
+                */
+                SendMail(smtp, from_mail, pass_mail, to_mail, "Файл создан", e.FullPath.ToString(), e.FullPath.ToString());
             }
             catch (Exception ex)
             {
@@ -134,10 +198,13 @@ namespace FileSystemWatcher_WF
                 if (yp != 10) { goto m1; }
                 else
                 {
+                    /*
                     str_txt = "Не удалось сохранить\r\n";
                     fwc.OpenText();
                     fwc.WriteText(str_txt);
                     fwc.CloseText();
+                    */
+                    SendMail(smtp, from_mail, pass_mail, to_mail, "Не получилось отправить файл", e.FullPath.ToString());
                 }
             }
         }
@@ -177,14 +244,50 @@ namespace FileSystemWatcher_WF
             {
                 if (e.KeyChar == 'R' || e.KeyChar == 'r')
                 {
-                    richTextBox1.Visible = true;
-                    button1.Visible = true;
-                    button2.Visible = true;
+                    OnVisible();
                 }
                 else 
                 {
                     ar = 0;
                 }
+            }
+        }
+
+        private void OnVisible()
+        {
+            richTextBox1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            label1.Visible = true;
+            textBox1.Visible = true;
+            this.Visible = true;
+        }
+
+        public static void SendMail(string smtpServer, string from, string password,
+                                        string mailto, string caption, string message, string attachFile = null)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(from);
+                mail.To.Add(new MailAddress(mailto));
+                mail.Subject = caption;
+                mail.Body = message;
+                if (!string.IsNullOrEmpty(attachFile))
+                    mail.Attachments.Add(new Attachment(attachFile));
+                SmtpClient client = new SmtpClient();
+                client.Host = smtpServer;
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(from.Split('@')[0], password);
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Send(mail);
+                // client.Dispose();
+                mail.Dispose();
+            }
+            catch (Exception e)
+            {
+                //throw new Exception("Mail.Send: " + e.Message);
             }
         }
     }
